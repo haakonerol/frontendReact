4438c3ede930befd719a4ba98f1bac205c92fdac
haakonerol <haakon.erol@gmail.com>
2025-05-09 12:56:56 +0200

    provider updated to 3 in 1 like create-wrap-consume context

--- context-api/src/context/LoginProvider.jsx
+++ context-api/src/context/LoginProvider.jsx
@@ -1,16 +1,23 @@
-import { createContext } from "react";
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 
+// 1. create context
 export const LoginContext = createContext();
 
+// 2. wrap the components with provider
 export default function LoginProvider(props) {
+
   const [signed, setSigned] = useState(false);
 
-  const values = { signed, setSigned }
+  const values = { signed, setSigned };
 
   return (
     <LoginContext.Provider value={values}>
       {props.children}
     </LoginContext.Provider>
   );
+}
+
+// 3. consume the context
+export const useLoginContext = () => {
+   return useContext(LoginContext)
 }
