9d1c52ef55256ea2abba007308c47fc138dcf135
haakonerol <haakon.erol@gmail.com>
2025-06-04 12:46:40 +0200

    login updated

--- react-router/src/pages/Login.jsx
+++ react-router/src/pages/Login.jsx
@@ -2,25 +2,22 @@ import { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-// import { LoginContext } from "../context/LoginContext";
-import { LoginContext } from "../context/LoginProvider";
+
 import { useNavigate } from "react-router";
 
 const Login = () => {
   const [user, setUser] = useState({ email: "", password: "" });
-  const {signed, setSigned } = useContext(LoginContext);
+  
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    user?.email && user?.password && setSigned(true);
-    setUser({ email: "", password: "" });
-    navigate(-1)
-    console.log(signed)
+    
+    
   };
-  console.log(user)
-  console.log(signed)
+  
+
   return (
     <Container>
       <h1 className="text-center mt-4">LOGIN PAGE</h1>
